9468ddbec2c3ecdd08dc1674284b44cb6e3c1c04
Nic Cheneweth <nic.cheneweth@thoughtworks.com>
2015-10-08 18:28:12 -0500

    removed template based spec tests

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -18,16 +18,6 @@ describe 'base provider' do
     expect(test_class.name).to eq 'vaquero_io_awschef'
   end
 
-  it '.create method should exist' do
-    expect(test_class.create('arguments', 'options', 'auth')).to eq \
-      'create function with arguments:arguments and options:options and auth:auth'
-  end
-
-  it '.destroy method should exist' do
-    expect(test_class.destroy('arguments', 'options', 'auth')).to eq \
-      'destroy function with arguments:arguments and options:options and auth:auth'
-  end
-
   it '.definition should load providerfile.yml without error' do
     expect { test_class.definition(providerfile) }.to_not raise_error
   end
